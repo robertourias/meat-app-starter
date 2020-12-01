1a827bbd3727154cb0423d958da867e088bd1a55
Roberto Nicoletti <roberto.urias@gmail.com>
2020-12-01 20:43:23 -0300

    Criando rotas

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'mt-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
