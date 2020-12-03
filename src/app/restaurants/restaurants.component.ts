4a6a2a8166616804ad5acf108520fa763cf923c4
Roberto Nicoletti <roberto.urias@gmail.com>
2020-12-02 22:34:17 -0300

    Criando componente card restaurant

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant/restant.model';
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
 })
 export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant[];
 
   constructor() { }
 
   ngOnInit() {
+    this.restaurants = [
+      {
+        id: "bread-bakery",
+        name: "Bread & Bakery",
+        category: "Bakery",
+        deliveryEstimate: "25m",
+        rating: 4.9,
+        imagePath: "assets/img/restaurants/breadbakery.png"
+      },
+      {
+        id: "burger-house",
+        name: "Burger House",
+        category: "Hamburgers",
+        deliveryEstimate: "100m",
+        rating: 3.5,
+        imagePath: "assets/img/restaurants/burgerhouse.png"
+      }
+    ];
   }
 
 }
